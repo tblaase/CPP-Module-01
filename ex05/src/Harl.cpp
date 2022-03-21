ce20919649e15d2a088e73cbc6a32a7bdba8613a
tblaase <tblaase@student.42heilbronn.de>
2022-03-21 16:53:59 +0100

    changed cerr to cout

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: tblaase <tblaase@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 15:00:25 by tblaase           #+#    #+#             */
-/*   Updated: 2022/03/18 18:23:48 by tblaase          ###   ########.fr       */
+/*   Updated: 2022/03/21 15:49:26 by tblaase          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,22 @@ Harl::~Harl()
 // Private Methods
 void	Harl::debug(void)
 {
-	std::cerr << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI really do!" << std::endl;
+	std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI really do!" << std::endl;
 }
 
 void	Harl::info(void)
 {
-	std::cerr << "[INFO]\nI cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger!\nIf you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger!\nIf you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning(void)
 {
-	std::cerr << "[WARNING]\nI think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[WARNING]\nI think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::error(void)
 {
-	std::cerr << "[ERROR]\nThis is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 // Public Methods
